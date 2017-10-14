74e030cf7e7457dd3a6e7faf6079c12bcd272525
joco25 <onungwej@gmail.com>
2017-10-14 09:40:17 +0100

    initial

---
+++ src/Notification.js
@@ -0,0 +1,13 @@
+export default{
+	template: '<div><h1>{{ notification }}</h1></div>',
+	
+	props:['message'],
+
+		
+ 	computed:{
+			notification(){
+				return this.message.toUpperCase();
+			}
+	}
+
+};
